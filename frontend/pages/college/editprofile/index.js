c07cbbe981b82320c21ee75cc7a6484c9bf3136e
vibhu <vibhu vibhusaini2k@gmail.com>
2023-07-04 15:08:35 +0530

    college-e-p

--- frontend/pages/college/editprofile/index.js
+++ frontend/pages/college/editprofile/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CollegeEditProfile from "../../../components/collegeEditProfile/collegeEditProfile";
-// import CollegeEditProfile from "../../components/collegeEditProfile";
 
 const CollegeEProfile = () => {
   return (
